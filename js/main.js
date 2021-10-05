b94d4781519e8ee0b346880e8442abdfca3a66a1
Tom Tzadka <91870433+TomTzadka@users.noreply.github.com>
2021-10-05 20:44:29 +0300

    end of second day

--- js/main.js
+++ js/main.js
@@ -13,14 +13,18 @@ var gGame = {
     secsPassed: 0
 }
 
+const MINE = '🧨';
+const FLAG = '🏴‍☠️';
+var currNumOfFlag = 2;
+var numOfMarkd = 0;
 
+var gIsTimerWork
+var gTotalMiliSeconds = 0.00;
+var gTimeStart;
 
-
-var gBomb = []
 // This is called when page loads
 
 function init() {
-    console.log('init');
     gBoard = buildBoard();
     renderBoard(gBoard, '.board-container')
 
@@ -31,7 +35,6 @@ function buildBoard() {
     for (var i = 0; i < gLevel.SIZE; i++) {
         board.push([]);
         for (var j = 0; j < gLevel.SIZE; j++) {
-
             board[i][j] = {
                 minesAroundCount: 0,
                 isShown: false,
@@ -44,37 +47,33 @@ function buildBoard() {
             }
         }
     }
-    console.log('board');
     return board
 }
 // Count mines around each cell and set the cell's minesAroundCount.
 function setMinesNegsCount(elCell) { /// start with board
     var minesCount = 0;
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
+    var cellCoords = getCallCoords(elCell.id)
+    var diff = 1
+    if (gBoard[cellCoords.i][cellCoords.j].isMine) return;
+    for (var i = cellCoords.i - diff; i <= cellCoords.i + 1; i++) {
 
-            if (elCell);
-
-
-            //         if(i === 0){
-            //             if (gBoard[i][j - 1].isMine) minesCount++; /// Top left
-            //         }
-            //         if (gBoard[i - 1][j - 1].isMine) minesCount++;
-
+        if (i < 0 || i >= gBoard.length) continue; /// check border
+        for (var j = cellCoords.j - diff; j <= cellCoords.j + 1; j++) {
+            if (j < 0 || j >= gBoard.length) continue; /// check border
+            var nextCell = { i: i, j: j }
+            if (nextCell.i === cellCoords.i && nextCell.j === cellCoords.j) {///dont check click
+                continue
+            } else {
+                // console.log(nextCell);
+                // console.log(gBoard[nextCell.i][nextCell.j].isMine);
+                if (gBoard[nextCell.i][nextCell.j].isMine) minesCount++;
+            }
         }
     }
-    // console.log('minesCount',minesCount);
-    // return minesCount;
-    console.log(elCell);
+    return minesCount;
 }
 
 
-// console.log('nextLocation', nextLocation);
-// var nextCell = gBoard[nextLocation.i][nextLocation.j];
-// if (nextCell === WALL) return
-// console.log('nextCell', nextCell);
-
-// return if cannot move
 
 // Render the board as a <table> to the page
 function renderBoard(board, selector) {
@@ -86,18 +85,13 @@ function renderBoard(board, selector) {
         for (var j = 0; j < board.length; j++) {
             // var cell = board[i][j];
             // var className = `cell cell${i}-${j}`;
-            strHTML += `<td class="cell cell${i}-${j}"  onclick="cellClicked(this, ${i}, ${j})">`
+            strHTML += `<td id="${i}-${j}" class="cell cell${i}-${j}"  onmousedown="whichButton(event, this)" oncontextmenu="event.preventDefault();">`
 
             if (gBoard[i][j].isMine) {
-                strHTML += `<span class="bomb">🧨</span></td>`
-
+                strHTML += `<span class="show"></span></td>`
             }
-
-            strHTML += `<span class="bomb"></span></td>`
-
-
+            strHTML += `<span class="show"></span></td>` /// empty
         }
-
         strHTML += '</tr>';
     }
     strHTML += '</tbody></table>';
@@ -105,36 +99,127 @@ function renderBoard(board, selector) {
     elContainer.innerHTML = strHTML;
 }
 // Called when a cell (td) is clicked
-function cellClicked(elCell, i, j) {
-    /// update model
-    gBoard[i][j].isShown = true;
-    // elCell.querySelector('.bomb').style.display = 'block'
-    // console.log(elCell.querySelector('.bomb'));
-
-    //count neighbor
-    setMinesNegsCount(elCell)
-    // elCell.remove()
-    console.log('i', i);
-    console.log('j', j);
+function cellClicked(elCell) {
+    if (!gIsTimerWork) {
+        gTimeStart = setInterval(setTime, 10);
+    }
+    gIsTimerWork = true;
+    cellMarked(elCell)
 
 }
 // Called on right click to mark a cell (suspected to be a mine) Search the web (and implement) how to hide the context menu on right click
 function cellMarked(elCell) {
+    numOfMarkd++
+    var cellIdx = getCallCoords(elCell.id);
+    var i = cellIdx.i
+    var j = cellIdx.j
 
+    if (!gBoard[i][j].isShown && !gBoard[i][j].isMarked) {
+        /// update model
+        gBoard[i][j].isShown = true;
+        var numOfMines = setMinesNegsCount(elCell)
+        gBoard[i][j].minesAroundCount = numOfMines
+
+        ///update dom
+        elCell.style.backgroundColor = 'rgb(204, 231, 225)'
+        if (gBoard[i][j].isMine) {
+            elCell.innerText = MINE
+            gameOver()
+        } else if (!gBoard[i][j].minesAroundCount) {
+            expandShown(elCell,i, j)
+        } else {
+            elCell.innerText = numOfMines;
+        }
+    }
+    if(numOfMarkd === 16){
+        victory()
+    }
+    
+}
+
+function putFlag(elCell) {
+    numOfMarkd++;
+    var cellIdx = getCallCoords(elCell.id);
+    var i = cellIdx.i;
+    var j = cellIdx.j;
+    if(!gBoard[i][j].isShown){
+        if (gBoard[i][j].isMarked) {
+            // update model
+            gBoard[i][j].isMarked = false;
+            currNumOfFlag++;
+            //update dom
+            elCell.innerText = '';
+            document.querySelector('#number-of-flag').innerText = currNumOfFlag;
+        } else {
+            // update model
+            gBoard[i][j].isMarked = true;
+            
+            currNumOfFlag--;
+            //update dom
+            elCell.innerText = FLAG;
+            document.querySelector('#number-of-flag').innerText = currNumOfFlag;
+
+        }   
+    }
 }
 // Game ends when all mines are marked, and all the other cells are shown
-function checkGameOver() {
+function gameOver() {
+    alert('Game Over')
+}
 
+function victory(){
+    alert('Victory')
 }
 // When user clicks a cell with no mines around, we need to open not only that cell,
 // but also its neighbors. NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
-function expandShown(board, elCell, i, j) {
 
+function whichButton(event, elCell) {
+    var mouseButton = event.which;
+    if (mouseButton === 1) cellMarked(elCell);/// Right click
+    if (mouseButton === 3) putFlag(elCell);/// Left click
+    
+}
+function expandShown(elCell) {
+    var cellIdx = getCallCoords(elCell.id);
+    var i = cellIdx.i;
+    var j = cellIdx.j;
+
+    var diff = 1;
+    for(var a = i - diff ; a <= i + 1 ; a++){
+        if (a < 0 || a >= gBoard.length) continue; /// check border
+        for(var b = j - diff ; b <= j + 1; b++){
+            if (b < 0 || b >= gBoard.length) continue; /// check border
+            if(a === i && b=== j ) continue; /// check me
+            if(gBoard[a][b].isShown) continue/// chech show
+            if(gBoard[a][b].isMine)continue; // check mine
+            var nextCell= document.querySelector(`.cell${a}-${b}`)
+            cellMarked(nextCell)
+                
+        }
+    }
 }
 
 
-function createMine() {
+
+
+
+
+
+
+function setTime() {
+    var secondsLabel = document.getElementById("seconds");
+    var miliSecondsLabel = document.getElementById("miliSeconds");
+    ++gTotalMiliSeconds;
+    miliSecondsLabel.innerText = pad(gTotalMiliSeconds % 60);
+    secondsLabel.innerHTML = pad(parseInt(gTotalMiliSeconds / 100));
 
 }
 
-
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
--- js/util.js
+++ js/util.js
@@ -1,24 +1,11 @@
 'use strict'
 
-function MouseClick(e) {
-    if (typeof e === 'object') {
-        switch (e.button) {
-            case 0:
-                console.log('Left button clicked.');
-                
-                break;
-            case 1:
-                console.log('Middle button clicked.');
-                break;
-            case 2:
-                console.log('Right button clicked.');
-                break;
-            default:
-                console.log(`Unknown button code: ${e.button}`);
-        }
-    }
+
+function getCallCoords(id) {
+    var coords = {};
+    var parts = id.split('-')
+    coords.i = +parts[0]
+    coords.j = +parts[1]
+    return coords
 }
-
-
-
 
